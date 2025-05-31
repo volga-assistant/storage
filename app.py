f3f6087f33d6b89ba72fc86d3fd674c6a85f1dab
sanjay suhrethu <suhrethuc@gmail.com>
2025-05-31 22:15:09 +0530

    the updated app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def download(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
