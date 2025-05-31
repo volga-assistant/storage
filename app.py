621ba93982bcd2dcb08c8dfdb58aeb43d87e5c23
sanjay suhrethu <suhrethuc@gmail.com>
2025-05-31 13:58:32 +0530

    Initial commit for cloud storage app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def home():
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    return redirect('/')
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
